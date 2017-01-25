8d8a12218cb77a67aab77b201cbd238680e05a7d
Simon Bennett <simon@bennett.im>
2017-01-25 10:30:45 +0000

    Force sentry to use the absolute git path

--- src/config/config.php
+++ src/config/config.php
@@ -4,5 +4,5 @@ return array(
     'dsn' => '',
 
     // capture release as git sha
-    // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 );
