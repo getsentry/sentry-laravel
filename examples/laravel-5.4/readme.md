cb025a6278650b36b6e2b9a43d89abc6ce52c742
David Cramer <dcramer@gmail.com>
2017-04-21 10:32:26 -0700

    Add laravel 5.4 example repo

---
+++ examples/laravel-5.4/readme.md
@@ -0,0 +1,12 @@
+Running this example:
+
+```
+# Create SQLite database:
+touch database/database.sqlite
+
+# Migrate schema
+php artisan migrate
+
+# Run webserver
+php artisan serve
+```
---
+++ examples/laravel-5.4/routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+function verifyCredentials()
+{
+    Log::info('Verifying credentials');
+    $user = DB::table('users')->where('name', 'John')->first();
+    throw new Exception('No credentials passed!');
+}
+
+
+function authenticateUser()
+{
+    Log::info('Authenticating the current user');
+    verifyCredentials();
+}
+
+
+Route::get('/', function () {
+    Log::info('Rendering a page thats about to error');
+    authenticateUser();
+})->name('index');
+
+Route::get('/welcome/{id}', 'HomeController@showWelcome')->name('welcome');
