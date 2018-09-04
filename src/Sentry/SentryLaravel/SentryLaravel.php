bca732f2947adc9f41811bde25c54f2fd0a7e700
Alex Bouma <me@alexbouma.me>
2018-09-04 15:48:59 +0200

    0.10.0

--- src/Sentry/SentryLaravel/SentryLaravel.php
+++ src/Sentry/SentryLaravel/SentryLaravel.php
@@ -4,7 +4,7 @@ namespace Sentry\SentryLaravel;
 
 class SentryLaravel
 {
-    const VERSION = '0.10.x-dev';
+    const VERSION = '0.10.0';
 
     public static function getClient($user_config)
     {
