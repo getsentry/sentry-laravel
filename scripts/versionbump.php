7aa576ba1c58c5d408cbe47ee354a10c81251bb0
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-01-16 14:52:11 +0100

    fix: Versionbump script

--- scripts/versionbump.php
+++ scripts/versionbump.php
@@ -3,4 +3,4 @@
 $re = '/\d+\.\d+.\d+(?:-\w+(?:\.\w+)?)?/m';
 
 $result = preg_replace($re, $argv[1], file_get_contents('../src/Sentry/Laravel/Version.php'));
-file_puts_contents('../src/Sentry/Laravel/Version.php', $result);
+file_put_contents('../src/Sentry/Laravel/Version.php', $result);
