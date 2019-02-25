79309255331bc96fd002ce7087aee420a94b6a9d
Alex Bouma <me@alexbouma.me>
2019-02-25 14:58:50 +0100

    Update default configs in examples

--- examples/laravel-5.4/config/sentry.php
+++ examples/laravel-5.4/config/sentry.php
@@ -1,16 +1,11 @@
 <?php
 
 return array(
-    'dsn' => env('SENTRY_LARAVEL_DSN', 'https://e9ebbd88548a441288393c457ec90441:399aaee02d454e2ca91351f29bdc3a07@sentry.io/3235'),
+    'dsn' => env('SENTRY_LARAVEL_DSN'),
 
     // capture release as git sha
-    // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
     // Capture bindings on SQL queries
     'breadcrumbs.sql_bindings' => true,
-
-    // Capture default user context
-    'user_context' => false,
-
-    'release' => '1.0',
 );
--- examples/lumen-5.2/config/sentry.php
+++ examples/lumen-5.2/config/sentry.php
@@ -1,8 +1,11 @@
 <?php
 
 return array(
-    'dsn' => 'https://e9ebbd88548a441288393c457ec90441:399aaee02d454e2ca91351f29bdc3a07@sentry.io/3235',
+    'dsn' => env('SENTRY_LARAVEL_DSN'),
+
+    // capture release as git sha
+    // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
     // Capture bindings on SQL queries
-    'breadcrumbs.sql_bindings' => false,
+    'breadcrumbs.sql_bindings' => true,
 );
