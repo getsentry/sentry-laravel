bbaa730306a16dcae155a45907423db583b91f51
leo108 <root@leo108.com>
2019-07-30 11:30:57 +0800

    Add more options to breadcrumbs configruation. #257

--- config/sentry.php
+++ config/sentry.php
@@ -8,10 +8,17 @@ return [
     // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
 
     'breadcrumbs' => [
+        // Capture Laravel logs in breadcrumbs
+        'logs' => true,
+
+        // Capture SQL queries in breadcrumbs
+        'sql_queries' => true,
 
         // Capture bindings on SQL queries logged in breadcrumbs
         'sql_bindings' => true,
 
+        // Capture queue job information in breadcrumbs
+        'queue_info' => true,
     ],
 
 ];
