86b4549d8c388c5237bcfdd2eb99673d4c645ff9
Daniel Griesser <daniel.griesser.86@gmail.com>
2019-08-23 09:11:58 +0200

    release: 1.1.1

--- src/Sentry/Laravel/Version.php
+++ src/Sentry/Laravel/Version.php
@@ -5,5 +5,5 @@ namespace Sentry\Laravel;
 final class Version
 {
     public const SDK_IDENTIFIER = 'sentry.php.laravel';
-    public const SDK_VERSION = '1.1.0';
+    public const SDK_VERSION = '1.1.1';
 }
