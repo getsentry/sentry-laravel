d1d6136a3ea49c940bda7d9d2e98bffd1d06c70e
Alex Bouma <me@alexbouma.me>
2019-10-15 21:34:37 +0200

    release: 1.4.0

--- src/Sentry/Laravel/Version.php
+++ src/Sentry/Laravel/Version.php
@@ -5,5 +5,5 @@ namespace Sentry\Laravel;
 final class Version
 {
     public const SDK_IDENTIFIER = 'sentry.php.laravel';
-    public const SDK_VERSION = '1.3.1';
+    public const SDK_VERSION = '1.4.0';
 }
