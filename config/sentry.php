78817c5a4176ee376873934a79bf54b45ee006b0
Alex Bouma <alex@bouma.me>
2022-10-11 14:11:33 +0200

    Remove extracting route name or controller for transaction names (#583)
    * Remove extracting route name or controller for transaction names
    
    * CS
    
    * Add changelog entry

--- config/sentry.php
+++ config/sentry.php
@@ -55,6 +55,4 @@ return [
 
     'traces_sample_rate' => (float)(env('SENTRY_TRACES_SAMPLE_RATE', 0.0)),
 
-    'controllers_base_namespace' => env('SENTRY_CONTROLLERS_BASE_NAMESPACE', 'App\\Http\\Controllers'),
-
 ];
