367fb90818a37bbd7183dcbbd474c3fead835332
Alex Bouma <alex@bouma.me>
2022-10-20 13:06:41 +0200

    Simplify even further

--- src/Sentry/Laravel/functions.php
+++ src/Sentry/Laravel/functions.php
@@ -9,10 +9,10 @@ use Sentry\Tracing\SpanContext;
 /**
  * Execute the given callable while wrapping it in a span added as a child to the current transaction and active span.
  *
- * If there is no transaction active this is a no-op and the scope passed to the trace callable will be `null`.
+ * If there is no transaction active this is a no-op and the scope passed to the trace callable will be unused.
  *
- * @param callable(\Sentry\State\Scope|null): mixed $trace   The callable that is going to be traced
- * @param \Sentry\Tracing\SpanContext               $context The context of the span to be created
+ * @param callable(\Sentry\State\Scope): mixed $trace   The callable that is going to be traced
+ * @param \Sentry\Tracing\SpanContext          $context The context of the span to be created
  *
  * @return mixed
  */
@@ -21,16 +21,14 @@ function trace(callable $trace, SpanContext $context)
     return SentrySdk::getCurrentHub()->withScope(function (Scope $scope) use ($context, $trace) {
         $parentSpan = $scope->getSpan();
 
-        // If there's no span set on the scope there is no transaction
-        // active currently. If that is the case we don't create a unused
-        // span and we immediately execute the callable and return the result
-        if ($parentSpan === null) {
-            return $trace(null);
+        // If there's a span set on the scope there is a transaction
+        // active currently. If that is the case we create a child span
+        // and set it on the scope. Otherwise we only execute the callable
+        if ($parentSpan !== null) {
+            $span = $parentSpan->startChild($context);
+
+            $scope->setSpan($span);
         }
-
-        $span = $parentSpan->startChild($context);
-
-        $scope->setSpan($span);
 
         return $trace($scope);
     });
