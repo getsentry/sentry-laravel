189361ae57cb61d18d52c477136c4fe142804ad6
Michael Hoffmann <michael.hoffmann@sentry.io>
2023-02-08 16:03:40 +0100

    feat: Install an JavaScript SDK during sentry:publish

--- src/Sentry/Laravel/Console/PublishCommand.php
+++ src/Sentry/Laravel/Console/PublishCommand.php
@@ -5,8 +5,10 @@ namespace Sentry\Laravel\Console;
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+use RuntimeException;
 use Sentry\Dsn;
 use Sentry\Laravel\ServiceProvider;
+use Symfony\Component\Process\Process;
 
 class PublishCommand extends Command
 {
@@ -53,10 +55,10 @@ class PublishCommand extends Command
             $arg['--dsn']              = $dsn;
         }
 
-        $testCommandPrompt = 'Want to send a test event?';
+        $testCommandPrompt = 'Do you want to send a test event to Sentry?';
 
         if ($this->confirm('Enable Performance Monitoring?', !$this->option('without-performance-monitoring'))) {
-            $testCommandPrompt = 'Want to send a test event & transaction?';
+            $testCommandPrompt = 'Do you want to send a test event & transaction to Sentry?';
 
             $env['SENTRY_TRACES_SAMPLE_RATE'] = '1.0';
 
@@ -72,6 +74,8 @@ class PublishCommand extends Command
                 return 1;
             }
         }
+
+        $this->askForJavaScriptSdk($dsn);
 
         $this->info('Publishing Sentry config...');
         $this->call('vendor:publish', ['--provider' => ServiceProvider::class]);
@@ -150,5 +154,116 @@ class PublishCommand extends Command
                 $this->error('The DSN is not valid, please make sure to paste a valid DSN!');
             }
         }
+    }
+
+    private function askForJavaScriptSdk(string $dsn): void
+    {
+        if ($this->confirm('Do you want to install one of our JavaScript SDKs?', true)) {
+            $framework = $this->choice(
+                'Which frontend framework are you using?',
+                ['Vue.js', 'React', 'Angular', 'Svelte'],
+            );
+
+            $snippet = '';
+
+            switch ($framework) {
+                case 'Vue.js':
+                    $this->updateNodePackages(function ($packages) {
+                        return [
+                            '@sentry/vue' => '^7.36.0',
+                        ] + $packages;
+                    });
+                    $snippet = 'import * as Sentry from "@sentry/vue";' . PHP_EOL . PHP_EOL;
+                    $snippet .= 'Sentry.init({' . PHP_EOL;
+                    $snippet .= '  app,' . PHP_EOL;
+                    $snippet .= '  dsn: "' . $dsn . '",' . PHP_EOL;
+                    $snippet .= '});' . PHP_EOL;
+                    break;
+                case 'React':
+                    $this->updateNodePackages(function ($packages) {
+                        return [
+                            '@sentry/react' => '^7.36.0',
+                        ] + $packages;
+                    });
+                    $snippet = 'import * as Sentry from "@sentry/react";' . PHP_EOL . PHP_EOL;
+                    $snippet .= 'Sentry.init({' . PHP_EOL;
+                    $snippet .= '  dsn: "' . $dsn . '",' . PHP_EOL;
+                    $snippet .= '});' . PHP_EOL;
+                    break;
+                case 'Angular':
+                    $this->updateNodePackages(function ($packages) {
+                        return [
+                            '@sentry/angular' => '^7.36.0',
+                        ] + $packages;
+                    });
+                    $snippet = 'import * as Sentry from "@sentry/angular";' . PHP_EOL . PHP_EOL;
+                    $snippet .= 'Sentry.init({' . PHP_EOL;
+                    $snippet .= '  dsn: "' . $dsn . '",' . PHP_EOL;
+                    $snippet .= '});' . PHP_EOL;
+                    break;
+                case 'Svelte':
+                    $this->updateNodePackages(function ($packages) {
+                        return [
+                            '@sentry/svelte' => '^7.36.0',
+                        ] + $packages;
+                    });
+                    $snippet = 'import * as Sentry from "@sentry/svelte";' . PHP_EOL . PHP_EOL;
+                    $snippet .= 'Sentry.init({' . PHP_EOL;
+                    $snippet .= '  dsn: "' . $dsn . '",' . PHP_EOL;
+                    $snippet .= '});' . PHP_EOL;
+                    break;
+            }
+
+            if (file_exists(base_path('pnpm-lock.yaml'))) {
+                $this->runCommands(['pnpm install']);
+            } elseif (file_exists(base_path('yarn.lock'))) {
+                $this->runCommands(['yarn install']);
+            } else {
+                $this->runCommands(['npm install']);
+            }
+
+            $this->newLine();
+            $this->info('Add the following snippet to your frontend code, like your index.js file.');
+            $this->newLine();
+            $this->line($snippet);
+        }
+    }
+
+    private function updateNodePackages(callable $callback)
+    {
+        if (! file_exists(base_path('package.json'))) {
+            return;
+        }
+
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
+
+        $packages['dependencies'] = $callback(
+            array_key_exists('dependencies', $packages) ? $packages['dependencies'] : [],
+            'dependencies'
+        );
+
+        ksort($packages['dependencies']);
+
+        file_put_contents(
+            base_path('package.json'),
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL
+        );
+    }
+
+    private function runCommands($commands)
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    '.$line);
+        });
     }
 }
