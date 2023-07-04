07b5e943bf28b9f48478722f4a4cbf179831d71c
Benedikt Franke <benedikt.franke@mll.com>
2023-07-04 16:43:59 +0200

    cleanup

--- src/Sentry/Laravel/Features/StorageIntegration.php
+++ src/Sentry/Laravel/Features/StorageIntegration.php
@@ -2,20 +2,11 @@
 
 namespace Sentry\Laravel\Features;
 
-use Illuminate\Cache\Events;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Filesystem\FilesystemManager;
-use Illuminate\Redis\Events as RedisEvents;
-use Illuminate\Redis\RedisManager;
-use Illuminate\Support\Str;
-use Sentry\Breadcrumb;
-use Sentry\Laravel\Features\Concerns\ResolvesEventOrigin;
-use Sentry\Laravel\Integration;
 use Sentry\Laravel\Tracing\Storage\TracingFilesystem;
-use Sentry\SentrySdk;
-use Sentry\Tracing\SpanContext;
 
 class StorageIntegration extends Feature
 {
