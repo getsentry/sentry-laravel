19d2bb0a1c50f7373eac1062a0ea153f7d22cb2d
Benedikt Franke <benedikt.franke@mll.com>
2023-07-04 16:47:08 +0200

    improve test

--- test/Sentry/Features/StorageIntegrationTest.php
+++ test/Sentry/Features/StorageIntegrationTest.php
@@ -8,7 +8,7 @@ use Sentry\Tracing\TransactionContext;
 
 class StorageIntegrationTest extends TestCase
 {
-    public function testCacheBreadcrumbForWriteAndHitIsRecorded(): void
+    public function testCreatesSpanFor(): void
     {
         $originalDisksLocalConfig = config('filesystems.disks.local');
         $this->resetApplicationWithConfig([
@@ -27,14 +27,25 @@ class StorageIntegrationTest extends TestCase
         $this->getCurrentScope()->setSpan($transaction);
 
         Storage::put('foo', 'bar');
+        $fooContent = Storage::get('foo');
+        Storage::assertExists('foo', 'bar');
 
         $spans = $transaction->getSpanRecorder()->getSpans();
-        $this->assertArrayHasKey(1, $spans);
 
+        $this->assertArrayHasKey(1, $spans);
         $span = $spans[1];
         $this->assertSame('file.put', $span->getOp());
         $this->assertSame(['path' => 'foo', 'options' => []], $span->getData());
 
-        Storage::assertExists('foo', 'bar');
+        $this->assertArrayHasKey(2, $spans);
+        $span = $spans[2];
+        $this->assertSame('file.get', $span->getOp());
+        $this->assertSame(['path' => 'foo'], $span->getData());
+        $this->assertSame('bar', $fooContent);
+
+        $this->assertArrayHasKey(3, $spans);
+        $span = $spans[3];
+        $this->assertSame('file.assertExists', $span->getOp());
+        $this->assertSame(['path' => 'foo'], $span->getData());
     }
 }
