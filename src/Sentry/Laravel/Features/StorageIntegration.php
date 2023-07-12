2d2f862ec33b5bde83b51170d9b786a0a2e353b3
Alex Bouma <alex@bouma.me>
2023-07-12 09:52:05 +0200

    Add some more guard rails with configuring filesystem driver

--- src/Sentry/Laravel/Features/StorageIntegration.php
+++ src/Sentry/Laravel/Features/StorageIntegration.php
@@ -6,11 +6,14 @@ use Illuminate\Contracts\Filesystem\Cloud as CloudFilesystem;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Filesystem\FilesystemManager;
+use RuntimeException;
 use Sentry\Laravel\Tracing\Storage\TracingCloudFilesystem;
 use Sentry\Laravel\Tracing\Storage\TracingFilesystem;
 
 class StorageIntegration extends Feature
 {
+    private const STORAGE_DRIVER_NAME = 'sentry';
+
     public function isApplicable(): bool
     {
         return $this->isTracingFeatureEnabled('storage');
@@ -19,21 +22,41 @@ class StorageIntegration extends Feature
     public function setup(): void
     {
         foreach (config('filesystems.disks') as $disk => $config) {
-            config(["filesystems.disks.{$disk}.original_driver" => $config['driver']]);
-            config(["filesystems.disks.{$disk}.driver" => 'sentry']);
+            $currentDriver = $config['driver'];
+
+            if ($currentDriver === self::STORAGE_DRIVER_NAME) {
+                continue;
+            }
+
+            config([
+                "filesystems.disks.{$disk}.driver" => self::STORAGE_DRIVER_NAME,
+                "filesystems.disks.{$disk}.sentry_disk_name" => $disk,
+                "filesystems.disks.{$disk}.sentry_original_driver" => $config['driver'],
+            ]);
         }
 
         $this->container()->afterResolving(FilesystemManager::class, static function (FilesystemManager $filesystemManager): void {
-            $filesystemManager->extend('sentry', function (Application $application, array $config) use ($filesystemManager): Filesystem {
-                $config['driver'] = $config['original_driver'];
-                unset($config['original_driver']);
+            $filesystemManager->extend(
+                self::STORAGE_DRIVER_NAME,
+                static function (Application $application, array $config) use ($filesystemManager): Filesystem {
+                    if (empty($config['sentry_original_driver'])) {
+                        throw new RuntimeException(sprintf('Missing `sentry_original_driver` config key for `%s` filesystem driver.', self::STORAGE_DRIVER_NAME));
+                    }
 
-                $originalFilesystem = $filesystemManager->build($config);
+                    if ($config['sentry_original_driver'] === self::STORAGE_DRIVER_NAME) {
+                        throw new RuntimeException(sprintf('`sentry_original_driver` for Sentry storage integration cannot be the `%s` driver.', self::STORAGE_DRIVER_NAME));
+                    }
 
-                return $originalFilesystem instanceof CloudFilesystem
-                    ? new TracingCloudFilesystem($originalFilesystem)
-                    : new TracingFilesystem($originalFilesystem);
-            });
+                    $config['driver'] = $config['sentry_original_driver'];
+                    unset($config['sentry_original_driver']);
+
+                    $originalFilesystem = $filesystemManager->build($config);
+
+                    return $originalFilesystem instanceof CloudFilesystem
+                        ? new TracingCloudFilesystem($originalFilesystem, $config['sentry_disk_name'] ?? 'unknown', $config['driver'])
+                        : new TracingFilesystem($originalFilesystem, $config['sentry_disk_name'] ?? 'unknown', $config['driver']);
+                }
+            );
         });
     }
 }
