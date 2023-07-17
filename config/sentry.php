8ca5b1d1e4a1f824117880b8a958db79529adcc9
Alex Bouma <alex@bouma.me>
2023-07-17 17:26:48 +0200

    Update default config file with `env` for everything and added docs

--- config/sentry.php
+++ config/sentry.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Sentry Laravel SDK configuration file.
+ *
+ * @see https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/
+ */
 return [
 
     // @see https://docs.sentry.io/product/sentry-basics/dsn-explainer/
@@ -9,72 +14,74 @@ return [
     // Example with dynamic git hash: trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD'))
     'release' => env('SENTRY_RELEASE'),
 
-    // When left empty or `null` the Laravel environment will be used
+    // When left empty or `null` the Laravel environment will be used (usually discovered from `APP_ENV` in your `.env`)
     'environment' => env('SENTRY_ENVIRONMENT'),
 
+    // Breadcrumb specific configuration
     'breadcrumbs' => [
-        // Capture Laravel logs in breadcrumbs
-        'logs' => true,
+        // Capture Laravel logs as breadcrumbs
+        'logs' => env('SENTRY_BREADCRUMBS_LOGS_ENABLED', true),
 
-        // Capture Laravel cache events in breadcrumbs
-        'cache' => true,
+        // Capture Laravel cache events (hits, writes etc.) as breadcrumbs
+        'cache' => env('SENTRY_BREADCRUMBS_CACHE_ENABLED', true),
 
-        // Capture Livewire components in breadcrumbs
-        'livewire' => true,
+        // Capture Livewire components like routes as breadcrumbs
+        'livewire' => env('SENTRY_BREADCRUMBS_LIVEWIRE_ENABLED', true),
 
-        // Capture SQL queries in breadcrumbs
-        'sql_queries' => true,
+        // Capture SQL queries as breadcrumbs
+        'sql_queries' => env('SENTRY_BREADCRUMBS_SQL_QUERIES_ENABLED', true),
 
-        // Capture bindings on SQL queries logged in breadcrumbs
-        'sql_bindings' => false,
+        // Capture SQL query bindings (parameters) in SQL query breadcrumbs
+        'sql_bindings' => env('SENTRY_BREADCRUMBS_SQL_BINDINGS_ENABLED', false),
 
-        // Capture queue job information in breadcrumbs
-        'queue_info' => true,
+        // Capture queue job information as breadcrumbs
+        'queue_info' => env('SENTRY_BREADCRUMBS_QUEUE_INFO_ENABLED', true),
 
-        // Capture command information in breadcrumbs
-        'command_info' => true,
+        // Capture command information as breadcrumbs
+        'command_info' => env('SENTRY_BREADCRUMBS_COMMAND_JOBS_ENABLED', true),
 
-        // Capture HTTP client requests information in breadcrumbs
-        'http_client_requests' => true,
+        // Capture HTTP client request information as breadcrumbs
+        'http_client_requests' => env('SENTRY_BREADCRUMBS_HTTP_CLIENT_REQUESTS_ENABLED', true),
     ],
 
+    // Performance monitoring specific configuration
     'tracing' => [
-        // Trace queue jobs as their own transactions
+        // Trace queue jobs as their own transactions (this enables tracing for queue jobs)
         'queue_job_transactions' => env('SENTRY_TRACE_QUEUE_ENABLED', false),
 
         // Capture queue jobs as spans when executed on the sync driver
-        'queue_jobs' => true,
+        'queue_jobs' => env('SENTRY_TRACE_QUEUE_JOBS_ENABLED', true),
 
         // Capture SQL queries as spans
-        'sql_queries' => true,
+        'sql_queries' => env('SENTRY_TRACE_SQL_QUERIES_ENABLED', true),
 
-        // Try to find out where the SQL query originated from and add it to the query spans
-        'sql_origin' => true,
+        // Capture where the SQL query originated from on the SQL query spans
+        'sql_origin' => env('SENTRY_TRACE_SQL_ORIGIN_ENABLED', true),
 
-        // Capture views as spans
-        'views' => true,
+        // Capture views rendered as spans
+        'views' => env('SENTRY_TRACE_VIEWS_ENABLED', true),
 
         // Capture Livewire components as spans
-        'livewire' => true,
+        'livewire' => env('SENTRY_TRACE_LIVEWIRE_ENABLED', true),
 
         // Capture HTTP client requests as spans
-        'http_client_requests' => true,
+        'http_client_requests' => env('SENTRY_TRACE_HTTP_CLIENT_REQUESTS_ENABLED', true),
 
         // Capture Redis operations as spans (this enables Redis events in Laravel)
         'redis_commands' => env('SENTRY_TRACE_REDIS_COMMANDS', false),
 
-        // Try to find out where the Redis command originated from and add it to the command spans
-        'redis_origin' => true,
+        // Capture where the Redis command originated from on the Redis command spans
+        'redis_origin' => env('SENTRY_TRACE_REDIS_ORIGIN_ENABLED', true),
 
-        // Indicates if the tracing integrations supplied by Sentry should be loaded
-        'default_integrations' => true,
+        // Enable tracing for requests without a matching route (404's)
+        'missing_routes' => env('SENTRY_TRACE_MISSING_ROUTES_ENABLED', false),
 
-        // Indicates that requests without a matching route should be traced
-        'missing_routes' => false,
-
-        // Indicates if the performance trace should continue after the response has been sent to the user until the application terminates
+        // Configures if the performance trace should continue after the response has been sent to the user until the application terminates
         // This is required to capture any spans that are created after the response has been sent like queue jobs dispatched using `dispatch(...)->afterResponse()` for example
-        'continue_after_response' => true,
+        'continue_after_response' => env('SENTRY_TRACE_CONTINUE_AFTER_RESPONSE', true),
+
+        // Enable the tracing integrations supplied by Sentry (recommended)
+        'default_integrations' => env('SENTRY_TRACE_DEFAULT_INTEGRATIONS_ENABLED', true),
     ],
 
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#send-default-pii
@@ -83,6 +90,7 @@ return [
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#traces-sample-rate
     'traces_sample_rate' => env('SENTRY_TRACES_SAMPLE_RATE') === null ? null : (float)env('SENTRY_TRACES_SAMPLE_RATE'),
 
+    // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#profiles-sample-rate
     'profiles_sample_rate' => env('SENTRY_PROFILES_SAMPLE_RATE') === null ? null : (float)env('SENTRY_PROFILES_SAMPLE_RATE'),
 
 ];
