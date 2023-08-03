8884fd2cae75907e4d02f2f8136814e8b1f91b29
Alex Bouma <alex@bouma.me>
2023-08-03 10:24:22 +0200

    Remove duplicated comment

--- src/Sentry/Laravel/Features/Storage/SentryFilesystemAdapter.php
+++ src/Sentry/Laravel/Features/Storage/SentryFilesystemAdapter.php
@@ -4,13 +4,6 @@ namespace Sentry\Laravel\Features\Storage;
 
 use Illuminate\Filesystem\FilesystemAdapter;
 
-/**
- * Decorates the underlying filesystem by wrapping all calls to it with tracing.
- *
- * Parameters such as paths, directories or options are attached to the span as data,
- * parameters that contain file contents are omitted due to potential problems with
- * payload size or sensitive data.
- */
 class SentryFilesystemAdapter extends FilesystemAdapter
 {
     use FilesystemDecorator;
