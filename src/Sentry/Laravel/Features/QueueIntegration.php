e1c66a27c91b32764dd377c59a4a10bbe8434811
Alex Bouma <alex@bouma.me>
2023-12-21 17:39:23 +0100

    Revert "Fix trace context being lost when finishing queue transaction"
    This reverts commit ff3c28d8377a94d9a3d564340ee60410ba2f5731.

--- src/Sentry/Laravel/Features/QueueIntegration.php
+++ src/Sentry/Laravel/Features/QueueIntegration.php
@@ -16,7 +16,6 @@ use Sentry\State\Scope;
 use Sentry\Tracing\PropagationContext;
 use Sentry\Tracing\SpanContext;
 use Sentry\Tracing\SpanStatus;
-use Sentry\Tracing\Transaction;
 use Sentry\Tracing\TransactionContext;
 use Sentry\Tracing\TransactionSource;
 
@@ -170,20 +169,8 @@ class QueueIntegration extends Feature
         $span = $this->maybePopSpan();
 
         if ($span !== null) {
-            $hub = SentrySdk::getCurrentHub();
-
-            $currentSpan = $hub->getSpan();
-
-            if ($span instanceof Transaction) {
-                $hub->setSpan($span);
-            }
-
-            $span->setStatus($status);
             $span->finish();
-
-            if ($span instanceof Transaction) {
-                $hub->setSpan($currentSpan);
-            }
+            $span->setStatus($status);
         }
     }
 
