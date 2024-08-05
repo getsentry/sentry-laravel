5b24122e110bb32dda3da5c6e6d3572218cf9235
Alex Bouma <alex@bouma.me>
2024-08-05 16:29:32 +0200

    Update comment

--- src/Sentry/Laravel/Tracing/TransactionFinisher.php
+++ src/Sentry/Laravel/Tracing/TransactionFinisher.php
@@ -12,9 +12,15 @@ class TransactionFinisher
         // We need to finish the transaction after the response has been sent to the client
         // so we register a terminating callback to do so, this allows us to also capture
         // spans that are created during the termination of the application like queue
-        // dispatched using dispatch(...)->afterResponse(). This middleware is called
-        // before the terminating callbacks so we are 99.9% sure to be the last one
-        // to run except if another terminating callback is registered after ours.
+        // dispatches using dispatch(...)->afterResponse() which are terminating
+        // callbacks themselfs just like we do below.
+        //
+        // This class is registered as a singleton in the container to ensure it's only
+        // instantiated once and the terminating callback is only registered once.
+        //
+        // It should be resolved from the container before the terminating callbacks are called.
+        // Good place is in the `terminate` callback of a middleware for example.
+        // This way we can be 99.9% sure to be the last ones to run.
         app()->terminating(function () {
             app(Middleware::class)->finishTransaction();
         });
