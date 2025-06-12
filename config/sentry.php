a47c48215680e3e39f1913b562fd7d273e9b2ace
Michi Hoffmann <cleptric@users.noreply.github.com>
2025-06-12 16:42:33 -0400

    Expose `enable_logs` option (#1004)

--- config/sentry.php
+++ config/sentry.php
@@ -32,6 +32,9 @@ return [
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#profiles-sample-rate
     'profiles_sample_rate' => env('SENTRY_PROFILES_SAMPLE_RATE') === null ? null : (float) env('SENTRY_PROFILES_SAMPLE_RATE'),
 
+    // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#enable-logs
+    'enable_logs' => env('SENTRY_ENABLE_LOGS', false),
+
     // @see: https://docs.sentry.io/platforms/php/guides/laravel/configuration/options/#send-default-pii
     'send_default_pii' => env('SENTRY_SEND_DEFAULT_PII', false),
 
