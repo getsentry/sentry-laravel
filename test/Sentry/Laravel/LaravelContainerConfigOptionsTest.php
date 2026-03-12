dcdc8ea0b394e69f609f76b708f4e44df8174fc1
Martin Linzmayer <martin.linzmayer@sentry.io>
2026-03-12 20:26:01 +0100

    feat: add strict-trace-continuation

--- test/Sentry/Laravel/LaravelContainerConfigOptionsTest.php
+++ test/Sentry/Laravel/LaravelContainerConfigOptionsTest.php
@@ -8,6 +8,42 @@ use Sentry\State\HubInterface;
 
 class LaravelContainerConfigOptionsTest extends TestCase
 {
+    public function testOrgIdIsNullByDefault(): void
+    {
+        $orgId = app(HubInterface::class)->getClient()->getOptions()->getOrgId();
+
+        $this->assertNull($orgId);
+    }
+
+    public function testOrgIdIsResolvedFromConfig(): void
+    {
+        $this->resetApplicationWithConfig([
+            'sentry.org_id' => 42,
+        ]);
+
+        $orgId = app(HubInterface::class)->getClient()->getOptions()->getOrgId();
+
+        $this->assertSame(42, $orgId);
+    }
+
+    public function testStrictTraceContinuationIsDisabledByDefault(): void
+    {
+        $enabled = app(HubInterface::class)->getClient()->getOptions()->isStrictTraceContinuationEnabled();
+
+        $this->assertFalse($enabled);
+    }
+
+    public function testStrictTraceContinuationIsResolvedFromConfig(): void
+    {
+        $this->resetApplicationWithConfig([
+            'sentry.strict_trace_continuation' => true,
+        ]);
+
+        $enabled = app(HubInterface::class)->getClient()->getOptions()->isStrictTraceContinuationEnabled();
+
+        $this->assertTrue($enabled);
+    }
+
     public function testLoggerIsNullByDefault(): void
     {
         $logger = app(HubInterface::class)->getClient()->getOptions()->getLogger();
