356e1c2ef8d982acbbe77bfecfb803e42cfe162a
Martin Linzmayer <martin.linzmayer@sentry.io>
2026-09-15 12:55:28 +0200

    feat(pii): add data collection for HTTP headers

---
+++ test/Sentry/Laravel/LaravelDataCollectionConfigOptionTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Sentry\Laravel\Tests\Laravel;
+
+use Sentry\DataCollection\DataCollectionOptions;
+use Sentry\Laravel\Tests\TestCase;
+
+class LaravelDataCollectionConfigOptionTest extends TestCase
+{
+    public function testDataCollectionDefaultsToLegacyMode(): void
+    {
+        $this->assertNull($this->getSentryClientFromContainer()->getOptions()->getDataCollection());
+
+        $this->resetApplicationWithConfig(['sentry.data_collection' => null]);
+
+        $this->assertNull($this->getSentryClientFromContainer()->getOptions()->getDataCollection());
+    }
+
+    public function testEmptyConfigurationActivatesSharedDefaults(): void
+    {
+        $this->resetApplicationWithConfig(['sentry.data_collection' => []]);
+
+        $dataCollection = $this->getDataCollection();
+
+        $this->assertSame('denyList', $dataCollection->getHttpHeaders()['request']['mode']);
+        $this->assertSame('denyList', $dataCollection->getHttpHeaders()['response']['mode']);
+    }
+
+    public function testPartialConfigurationRetainsDefaultsForOtherDirection(): void
+    {
+        $this->resetApplicationWithConfig([
+            'sentry.data_collection' => [
+                'http_headers' => [
+                    'request' => ['mode' => 'off'],
+                ],
+            ],
+        ]);
+
+        $headers = $this->getDataCollection()->getHttpHeaders();
+
+        $this->assertSame('off', $headers['request']['mode']);
+        $this->assertSame('denyList', $headers['response']['mode']);
+    }
+
+    public function testHeaderShorthandIsForwardedToBothDirections(): void
+    {
+        $this->resetApplicationWithConfig([
+            'sentry.data_collection' => [
+                'http_headers' => ['mode' => 'allowList', 'terms' => ['x-request-id']],
+            ],
+        ]);
+
+        $headers = $this->getDataCollection()->getHttpHeaders();
+
+        $this->assertSame($headers['request'], $headers['response']);
+        $this->assertSame(['mode' => 'allowList', 'terms' => ['x-request-id']], $headers['request']);
+    }
+
+    public function testInvalidHeaderConfigurationUsesSharedResolverFallbacks(): void
+    {
+        $this->resetApplicationWithConfig([
+            'sentry.data_collection' => [
+                'http_headers' => [
+                    'request' => ['mode' => 'invalid', 'terms' => 'invalid'],
+                ],
+            ],
+        ]);
+
+        $headers = $this->getDataCollection()->getHttpHeaders();
+
+        $this->assertSame('denyList', $headers['request']['mode']);
+        $this->assertSame([], $headers['request']['terms']);
+        $this->assertSame('denyList', $headers['response']['mode']);
+    }
+
+    /**
+     * @dataProvider sendDefaultPiiProvider
+     */
+    public function testConfiguredPolicyIsIndependentOfSendDefaultPii(bool $sendDefaultPii): void
+    {
+        $this->resetApplicationWithConfig([
+            'sentry.send_default_pii' => $sendDefaultPii,
+            'sentry.data_collection' => [
+                'http_headers' => ['mode' => 'off'],
+            ],
+        ]);
+
+        $headers = $this->getDataCollection()->getHttpHeaders();
+
+        $this->assertSame('off', $headers['request']['mode']);
+        $this->assertSame('off', $headers['response']['mode']);
+    }
+
+    public static function sendDefaultPiiProvider(): iterable
+    {
+        yield [false];
+        yield [true];
+    }
+
+    private function getDataCollection(): DataCollectionOptions
+    {
+        $dataCollection = $this->getSentryClientFromContainer()->getOptions()->getDataCollection();
+
+        $this->assertInstanceOf(DataCollectionOptions::class, $dataCollection);
+
+        return $dataCollection;
+    }
+}
